be6c0a698002ce7832f3187a2197388e882de072
Davidemeka1234 <elchapodave@gmail.com>
2021-07-12 16:36:23 -0700

    UNACHUKWU DAVID EMEKA ASSIGNMENT

---
+++ EEG 323 QUESTION 1/assignment 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   string course;
+            string studentProgram;
+
+            Console.WriteLine("Are you a full-time student or a part-time student");
+
+            studentProgram = Console.ReadLine();
+
+            if (studentProgram == "full-time")
+            {
+                Console.WriteLine("What is your course of study");
+                course = Console.ReadLine();
+
+                Console.WriteLine($"{course} is a good course");
+            }
+
+            else if (studentProgram == "part-time")
+            {
+                Console.WriteLine("How many credits are you taking");
+
+            int credits = Convert.ToInt32(Console.ReadLine());
+
+            if (credits > 6)
+        Console.WriteLine("Too much units for for a part-time student");
+        
+        else{
+             Console.WriteLine("proceed");
+             }
+        
+        }
+                
+            
+        
+        
+        }
+    }
+}
